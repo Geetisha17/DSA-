4ac5624ce5cc65000a1f7ad05f7619f9a0ea02ff
Geetisha Tandon <93433422+Geetisha17@users.noreply.github.com>
2024-08-07 19:56:16 +0530

    [547] [Time Beats: 95.56%] [Memory Beats: 64.58%] - LeetPush

--- 547-Number-of-Provinces.cpp
+++ 547-Number-of-Provinces.cpp
@@ -1,43 +1,87 @@
-class Solution {
-    void helper(int node,vector<int>&vis,vector<vector<int>>&adj)
+class DisjointSet{
+    vector<int>parent,rank,size;
+    public:
+    DisjointSet(int n)
     {
-        vis[node]=1;
-
-        for(auto it:adj[node])
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++) parent[i]=i;
+    }
+    int findUPar(int node)
+    {
+        if(node==parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    
+    void unionByRank(int u,int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        
+        if(ulp_u == ulp_v) return;
+        else if(rank[ulp_u] < rank[ulp_v])
         {
-            if(!vis[it])
-            {
-                helper(it,vis,adj);
-            }
+            parent[ulp_u] = ulp_v;
+        }else if(rank[ulp_u] > rank[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
+        }else{
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
         }
     }
+    void unionBySize(int u,int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        
+        if(ulp_u == ulp_v) return;
+        else if(rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }else if(rank[ulp_u] > rank[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }else{
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+};
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& graph) {
-        int n=isConnected.size();
-        vector<int>vis(n+1,0);
-        vector<vector<int>>adj(n);
+        int n=graph.size();
+        vector<int>adj[n];
 
         for(int i=0;i<graph.size();i++)
         {
-            for(int j=0;j<graph[i].size();j++)
+            for(int j=0;j<graph[0].size();j++)
             {
-                if(graph[i][j]==1 && i!=j)
+                if(graph[i][j]==1)
                 {
-                adj[i].push_back(j);
-                adj[j].push_back(i);
+                    ds.unionBySize(i,j);
                 }
             }
         }
 
+        DisjointSet ds(n);
         int cnt=0;
-        for(int i=1;i<=n;i++)
+
+        for(int i=0;i<n;i++)
         {
-            if(!vis[i])
+            for(auto it:adj[i])
             {
-                cnt++;
-                helper(i,vis,adj);
+                if(ds.findUPar(i) != ds.findUPar(it))
+                {
+                    cnt++;
+                }
             }
         }
-        return cnt;
     }
 };
