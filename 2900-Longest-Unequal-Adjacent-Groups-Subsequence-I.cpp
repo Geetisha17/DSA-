3879523b8ab189358edcafd0e4ae7cbe69aa9b8f
Geetisha Tandon <93433422+Geetisha17@users.noreply.github.com>
2024-08-23 16:55:18 +0530

    [2900] [Time Beats: 9.26%] [Memory Beats: 16.62%] - LeetPush

--- 2900-Longest-Unequal-Adjacent-Groups-Subsequence-I.cpp
+++ 2900-Longest-Unequal-Adjacent-Groups-Subsequence-I.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        int x = groups[0];
-        vector<string>ans;
-        ans.push_back(words[0]);
-        
-        for(int i=1;i<groups.size();i++)
+        int n=words.size();
+        vector<int>dp(n+1,0);
+        vector<int>len(n+1,-1);
+
+        for(int i=0;i<n;i++)
         {
-            if(groups[i] != x)
+            for(int j=0;j<i;j++)
             {
-                x=groups[i];
-                ans.push_back(words[i]);
+                if(groups[i] != groups[j] && 1+dp[j] > dp[i])
+                {
+                    dp[i] = 1+dp[j];
+                    len[i] = j;
+                }
             }
         }
+        int maxi = 0;
+        for (int i = 1; i < n; i++) {
+            if (dp[i] > dp[maxi]) {
+                maxi = i;
+            }
+        }
+        
+        vector<string> ans;
+        while (maxi != -1) {
+            ans.push_back(words[maxi]);
+            maxi = len[maxi];
+        }
+        
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
