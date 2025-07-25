1b4e27e3e09cd7ff7a1866237444b54f16f6c434
Geetisha Tandon <93433422+Geetisha17@users.noreply.github.com>
2025-07-25 14:38:40 +0530

    [315] [Time Beats: 24.63%] [Memory Beats: 18.55%] - LeetPush

--- Count-of-Smaller-Numbers-After-Self.cpp
+++ Count-of-Smaller-Numbers-After-Self.cpp
@@ -1,48 +1,45 @@
 class Solution {
-    void merge(vector<pair<int,int>>&nums,int l,int r,int mid,vector<int>&ans)
+    void merge(vector<pair<int,int>>&nums,vector<int>&ans,int l,int mid,int r)
     {
         int i=l;int j=mid+1;
-        vector<pair<int,int>>temp(r-l+1);
-        int k=0;
+        vector<pair<int,int>>temp;
 
         while(i<=mid && j<=r)
         {
             if(nums[i].first > nums[j].first)
             {
                 ans[nums[i].second] += (r-j+1);
-                temp[k++]=  nums[i++];
+                temp.push_back(nums[i++]);
             }
-            else{
-                temp[k++] = nums[j++];
-            }
+            else
+                temp.push_back(nums[j++]);
         }
         while(i<=mid)
-            temp[k++] = nums[i++];
-        while(j<=r) 
-            temp[k++] = nums[j++];
-        
-        for (int i = l; i <= r; i++) {
-            nums[i] = temp[i - l];
-        }
-    }
-    void mergeSort(vector<pair<int,int>>&nums,int l,int r,vector<int>&ans)
-    {
-        if(l>=r) return;
+            temp.push_back(nums[i++]);
+        while(j<=r)
+            temp.push_back(nums[j++]);
 
+        int k=0;
+        for(int st=l;st<=r;st++)
+            nums[st] = temp[k++];
+    }
+    void mergeSort(vector<pair<int,int>>&vec,vector<int>&ans,int l,int r)
+    {
+        if(l>=r) return ;
         int mid = (l+r)/2;
-        mergeSort(nums,l,mid,ans);
-        mergeSort(nums,mid+1,r,ans);
-        merge(nums,l,r,mid,ans);
+        mergeSort(vec,ans,l,mid);
+        mergeSort(vec,ans,mid+1,r);
+        merge(vec,ans,l,mid,r);
     }
 public:
     vector<int> countSmaller(vector<int>& nums) {
+        vector<pair<int,int>>vec;
         int n=nums.size();
         vector<int>ans(n,0);
-        vector<pair<int,int>>vec;
-        
         for(int i=0;i<nums.size();i++)
             vec.push_back({nums[i],i});
-        mergeSort(vec,0,n-1,ans);
+
+        mergeSort(vec,ans,0,n-1);
         return ans;
     }
 };
